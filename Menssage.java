154f052285396cbeb388ce503f6fb4b111e349ac
MICHAELVM01 <michaelino02@gmail.com>
2022-12-16 21:58:13 -0500

    primera

---
+++ Menssage.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Menssage {
+        public static void main(String[] args) {
+                MyIcon icon = new MyIcon();
+                JOptionPane.showMessageDialog(null, "Te amo porque soy feliz amándote (❤ ω ❤)", "❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Mi lugar favorito en el mundo es contigo (˘︶˘).｡*♡", "❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Sos la unica que saca la mejor version de mi ◕‿◕", "❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Estoy completamente enamorado de vos (❤ ω ❤)", " ❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Te Pienso y te deseo cada instante ᕙ(^▿^-ᕙ)", "❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Que seas miaaa!!! (˘︶˘).｡*♡", "❤ Te Amo Mariel ❤",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "No te cambiaria por nada en el mundooooo!!! ♡(˃͈ દ ˂͈ ༶ )",
+                                "∿ Te Amo Mariel ❤", JOptionPane.DEFAULT_OPTION, icon);
+        }
+}
